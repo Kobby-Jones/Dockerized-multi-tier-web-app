0374b10fc4e764b003d6a094a71da927be34644c
Kobby Jones <kobbyj099@gmail.com>
2024-12-10 17:34:41 +0000

    Configured th docker-composed file

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
 # backend/app.py
 from flask import Flask, jsonify
+import os
+import mysql.connector
 
 app = Flask(__name__)
 
+db_connection = mysql.connector.connect(
+    host=os.environ.get('DB_HOST'),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD'),
+    database=os.environ.get('DB_NAME')
+)
+
+cursor = db_connection.cursor()
+
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from the Backend!"})
-
+    cursor.execute("SELECT 'Hello form the database'")
+    result = cursor.fetchone()
+    return jsonify({'message': result[0]})
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
